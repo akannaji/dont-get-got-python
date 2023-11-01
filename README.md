f084ac59167f31ff4e413f760132685b7a3a06ed
Aka Nnaji <akamonye.nnaji@gmail.com>
2023-11-01 01:37:43 -0500

    added readme, added quit or restart prormpt

---
+++ README.md
@@ -0,0 +1,15 @@
+# Avoid the Blocks! Game
+
+**Avoid the Blocks!** is a simple game where the player controls a blue square aiming to dodge the falling red blocks. Survive as long as you can!
+
+## How to Run
+
+1. Ensure you have Python 3.x installed.
+2. Install the required library:
+   ```bash
+   pip install pygame
+3. Run the game script:
+    ```bash
+   python avoid_the_blocks.py
+
+Use the left and right arrow keys to navigate the blue square and avoid collisions.
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ pygame.init()
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
 # Screen dimensions
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Don't Get Got!")
+pygame.display.set_caption("Avoid the Blocks!")
 
-# Define the Player class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -41,7 +41,6 @@ class Player(pygame.sprite.Sprite):
     def stop(self):
         self.change_x = 0
 
-# Define the Block class
 class Block(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -58,48 +57,79 @@ class Block(pygame.sprite.Sprite):
             self.rect.y = random.randint(-100, -40)
             self.rect.x = random.randint(0, WIDTH)
 
-player = Player()
-all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
 
-blocks = pygame.sprite.Group()
+# ... [ Player and Block class definitions remain unchanged ]
 
-for i in range(5):
-    block = Block()
-    all_sprites.add(block)
-    blocks.add(block)
-
-clock = pygame.time.Clock()
-running = True
-score = 0
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.go_left()
-            elif event.key == pygame.K_RIGHT:
-                player.go_right()
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                player.stop()
-
-    all_sprites.update()
-
-    block_hits = pygame.sprite.spritecollide(player, blocks, False)
-    if block_hits:
-        running = False
-
+def display_game_over():
+    """Display the game over message and return player's choice (True for try again, False for quit)."""
     screen.fill(WHITE)
-    all_sprites.draw(screen)
-
-    score += 1
-    score_text = pygame.font.SysFont('arial', 25).render(f'Score: {score}', True, (0, 0, 0))
-    screen.blit(score_text, (10, 10))
-
+    font = pygame.font.SysFont('arial', 50)
+    game_over_text = font.render("Game Over", True, BLACK)
+    instruction_text = font.render("Press 'T' to Try Again or 'Q' to Quit", True, BLACK)
+    
+    screen.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 3))
+    screen.blit(instruction_text, (WIDTH // 2 - instruction_text.get_width() // 2, HEIGHT // 2))
+    
     pygame.display.flip()
-    clock.tick(60)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_t:
+                    return True
+                elif event.key == pygame.K_q:
+                    return False
+
+# Game loop
+while True:
+    player = Player()
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+
+    blocks = pygame.sprite.Group()
+    for i in range(5):
+        block = Block()
+        all_sprites.add(block)
+        blocks.add(block)
+
+    clock = pygame.time.Clock()
+    running = True
+    score = 0
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.go_left()
+                elif event.key == pygame.K_RIGHT:
+                    player.go_right()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player.stop()
+
+        all_sprites.update()
+
+        block_hits = pygame.sprite.spritecollide(player, blocks, False)
+        if block_hits:
+            running = False
+
+        screen.fill(WHITE)
+        all_sprites.draw(screen)
+
+        score += 1
+        score_text = pygame.font.SysFont('arial', 25).render(f'Score: {score}', True, (0, 0, 0))
+        screen.blit(score_text, (10, 10))
+
+        pygame.display.flip()
+        clock.tick(60)
+    
+    # After the inner game loop finishes (i.e., player collides), prompt for retry
+    if not display_game_over():
+        break
 
 pygame.quit()
